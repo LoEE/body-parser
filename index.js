0b8deea860b44d29e3f367544e18c9a7d43e5072
Jakub Piotr Cłapa <jpc@loee.pl>
2016-09-24 18:10:22 +0200

    Eliminate dynamic code loading.

--- index.js
+++ index.js
@@ -39,27 +39,10 @@ var parsersDir = path.join(__dirname, 'lib', 'types')
  * Auto-load bundled parsers with getters.
  */
 
-fs.readdirSync(parsersDir).forEach(function onfilename(filename) {
-  if (!/\.js$/.test(filename)) return
-
-  var loc = path.resolve(parsersDir, filename)
-  var mod
-  var name = path.basename(filename, '.js')
-
-  function load() {
-    if (mod) {
-      return mod
-    }
-
-    return mod = require(loc)
-  }
-
-  Object.defineProperty(exports, name, {
-    configurable: true,
-    enumerable: true,
-    get: load
-  })
-})
+exports.urlencoded = require('./lib/types/urlencoded');
+exports.text = require('./lib/types/text');
+exports.raw = require('./lib/types/raw');
+exports.json = require('./lib/types/json');
 
 /**
  * Create a middleware to parse json and urlencoded bodies.
